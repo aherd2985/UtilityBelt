035370e78d1fc1cdcdb5d8812eae144c9df5fde0
Andrew Herd <aherd2985@gmail.com>
2020-10-01 20:53:17 -0500

    Created new method to hit api and display a cat fact

---
+++ Models/CatFactModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UtilityBelt.Models
+{
+  class CatFactModel
+  {
+    public bool used { get; set; }
+    public string source { get; set; }
+    public string type { get; set; }
+    public bool deleted { get; set; }
+    public string _id { get; set; }
+    public DateTime updatedAt { get; set; }
+    public DateTime createdAt { get; set; }
+    public string user { get; set; }
+    public string text { get; set; }
+    public int __v { get; set; }
+    public Status status { get; set; }
+  }
+  public class Status
+  {
+    public bool verified { get; set; }
+    public int sentCount { get; set; }
+  }
+}
