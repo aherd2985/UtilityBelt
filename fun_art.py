24a59950d848a180752804e28af1970a83968f31
Ayush Raj <beast@localhost.localdomain>
2020-10-04 03:14:17 +0530

    added a ascii art

---
+++ fun_art.py
@@ -0,0 +1,14 @@
+import  pyfiglet
+
+
+#for Hacktober fest
+
+hacktober = pyfiglet.figlet_format("HACKTOBER FEST 2020", font="doh")
+
+print(hacktober)
+
+fun_console = pyfiglet.figlet_format("Fun Console Application", font="bulbhead")
+print(fun_console)
+welcome = pyfiglet.figlet_format("Welcome to Hacktober Fest 2020\n feel free to contribute with open source projects", font="slant")
+
+print(welcome)
