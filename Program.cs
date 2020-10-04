a9cce29c263dda9f999fc1f563e35b701eb54108
Jose Enrique Peraza Santamaria <30508442+jperaza1@users.noreply.github.com>
2020-10-04 01:11:46 -0600

    adding each menu option in its own method
    Methods were created for each option

--- Program.cs
+++ Program.cs
@@ -1,4 +1,40 @@
-﻿using Microsoft.Extensions.Options;
+Skip to content
+Search or jump to…
+
+Pull requests
+Issues
+Marketplace
+Explore
+ 
+@jperazas208 
+Learn Git and GitHub without any code!
+Using the Hello World guide, you’ll start a branch, write comments, and open a pull request.
+
+
+jperazas208
+/
+UtilityBelt
+forked from jperaza1/UtilityBelt
+0
+03
+Code
+Pull requests
+Actions
+Projects
+Wiki
+Security
+Insights
+Settings
+UtilityBelt/Program.cs
+@jperaza1
+jperaza1 adding each menu option in its own method
+Latest commit a768219 4 minutes ago
+ History
+ 3 contributors
+@aherd2985@jperaza1@jperazas208
+211 lines (181 sloc)  7.71 KB
+ 
+using Microsoft.Extensions.Options;
 using System;
 using System.Net;
 using System.Net.Mail;
@@ -48,124 +84,38 @@ namespace UtilityBelt
                 case "1":
                 case "port":
                 case "port scanner":
-                    Console.WriteLine("Please enter a domain:");
-                    string domain = Console.ReadLine().ToLower();
-                    Console.WriteLine("Please enter a starting Port Number:");
-                    int lowPort = int.Parse(Console.ReadLine());
-                    Console.WriteLine("Please enter an ending Port Number:");
-                    int highPort = int.Parse(Console.ReadLine());
-
-                    PortScanner.Scanner(domain, lowPort, highPort);
+                    portScanner();
                     break;
 
                 case "2":
                 case "ssms":
                 case "text":
                 case "text message":
-
-                    Console.WriteLine("");
-                    Console.WriteLine("Input number to receive message:");
-
-                    string send2Number = Console.ReadLine().ToLower();
-
-                    SmtpClient client = new SmtpClient("smtp.gmail.com", 587)
-                    {
-                        Credentials = new NetworkCredential(options.Value.Email, options.Value.EmailPassword),
-                        EnableSsl = true
-                    };
-
-                    MailMessage message = new MailMessage();
-                    message.From = new MailAddress(options.Value.Email);
-
-                    // assuming at&t for now
-                    // todo: add other carriers... fun
-
-                    message.To.Add(new MailAddress(send2Number + "@txt.att.net"));
-                    message.Subject = "This is my subject";
-                    message.Body = "This is the content";
-
-                    client.Send(message);
-                    Console.WriteLine("Sent");
-                    Console.ReadLine();
+                    TextMessage(options);
                     break;
-
                 case "3":
                 case "random chuck norris joke":
                 case "chuck norris joke":
                 case "chuck norris":
                 case "joke":
-                    string content = string.Empty;
-                    string url = "https://api.chucknorris.io/jokes/random";
-                    WebRequest myReq = WebRequest.Create(url);
-
-                    using (WebResponse wr = myReq.GetResponse())
-                    using (Stream receiveStream = wr.GetResponseStream())
-                    using (StreamReader sReader = new StreamReader(receiveStream, Encoding.UTF8))
-                        content = sReader.ReadToEnd();
-                    ChuckJokeModel chuckJoke = JsonSerializer.Deserialize<ChuckJokeModel>(content);
-                    Console.WriteLine("");
-                    Console.ForegroundColor = ConsoleColor.Yellow;
-                    Console.WriteLine(chuckJoke.value);
-                    Console.WriteLine("");
+                    RandomChuckNorrisJoke();
                     break;
-
                 case "4":
                 case "cat fact":
                 case "cat":
-                    string catUrl = "https://cat-fact.herokuapp.com/facts/random";
-                    WebRequest catReq = WebRequest.Create(catUrl);
-
-                    using (WebResponse wr = catReq.GetResponse())
-                    using (Stream receiveStream = wr.GetResponseStream())
-                    using (StreamReader sReader = new StreamReader(receiveStream, Encoding.UTF8))
-                        content = sReader.ReadToEnd();
-                    CatFactModel catFact = JsonSerializer.Deserialize<CatFactModel>(content);
-                    Console.WriteLine("");
-                    if (catFact.status.verified)
-                        Console.ForegroundColor = ConsoleColor.Yellow;
-                    else
-                        Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine(catFact.text);
-                    Console.WriteLine("");
+                    CatFact();
                     break;
 
                 case "5":
                 case "bitcoin prices":
                 case "bitcoin":
-                    string bitUrl = "https://api.coindesk.com/v1/bpi/currentprice.json";
-                    WebRequest bitReq = WebRequest.Create(bitUrl);
-
-                    using (WebResponse wr = bitReq.GetResponse())
-                    using (Stream receiveStream = wr.GetResponseStream())
-                    using (StreamReader sReader = new StreamReader(receiveStream, Encoding.UTF8))
-                        content = sReader.ReadToEnd();
-                    BitcoinPrice bitFact = JsonSerializer.Deserialize<BitcoinPrice>(content);
-                    Console.WriteLine("");
-                    Console.ForegroundColor = ConsoleColor.Yellow;
-                    Console.WriteLine("As Of - " + bitFact.time.updated);
-                    Console.WriteLine("USD - $ " + bitFact.bpi.USD.rate);
-                    Console.WriteLine("");
+                    BitcoinPrices();
                     break;
 
                 case "6":
                 case "who is in space":
                 case "space":
-                    string sapcePeopleUrl = "http://api.open-notify.org/astros.json";
-                    WebRequest sapcePeopleReq = WebRequest.Create(sapcePeopleUrl);
-
-                    using (WebResponse wr = sapcePeopleReq.GetResponse())
-                    using (Stream receiveStream = wr.GetResponseStream())
-                    using (StreamReader sReader = new StreamReader(receiveStream, Encoding.UTF8))
-                        content = sReader.ReadToEnd();
-                    SpacePersonModel sapcePeopleFact = JsonSerializer.Deserialize<SpacePersonModel>(content);
-                    Console.WriteLine("");
-                    Console.ForegroundColor = ConsoleColor.Yellow;
-                    Console.WriteLine("There are " + sapcePeopleFact.people.Count() + " in space right now!");
-                    foreach (SpacePerson spacePerson in sapcePeopleFact.people)
-                    {
-                        Console.WriteLine(spacePerson.name + " is in " + spacePerson.craft);
-                    }
-                    Console.WriteLine("");
+                    Space();
                     break;
                 default:
                     Console.WriteLine("Please make a valid option");
@@ -174,5 +124,136 @@ namespace UtilityBelt
 
             }
         }
+
+        static void portScanner()
+        {
+            Console.WriteLine("Please enter a domain:");
+            string domain = Console.ReadLine().ToLower();
+            Console.WriteLine("Please enter a starting Port Number:");
+            int lowPort = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter an ending Port Number:");
+            int highPort = int.Parse(Console.ReadLine());
+
+            PortScanner.Scanner(domain, lowPort, highPort);
+        }
+
+        static void TextMessage(IOptions<SecretsModel> options)
+        {
+            Console.WriteLine("");
+            Console.WriteLine("Input number to receive message:");
+
+            string send2Number = Console.ReadLine().ToLower();
+
+            SmtpClient client = new SmtpClient("smtp.gmail.com", 587)
+            {
+                Credentials = new NetworkCredential(options.Value.Email, options.Value.EmailPassword),
+                EnableSsl = true
+            };
+
+            MailMessage message = new MailMessage();
+            message.From = new MailAddress(options.Value.Email);
+
+            // assuming at&t for now
+            // todo: add other carriers... fun
+
+            message.To.Add(new MailAddress(send2Number + "@txt.att.net"));
+            message.Subject = "This is my subject";
+            message.Body = "This is the content";
+
+            client.Send(message);
+            Console.WriteLine("Sent");
+            Console.ReadLine();
+        }
+
+        static void RandomChuckNorrisJoke()
+        {
+            string content = string.Empty;
+            string url = "https://api.chucknorris.io/jokes/random";
+            WebRequest myReq = WebRequest.Create(url);
+
+            using (WebResponse wr = myReq.GetResponse())
+            using (Stream receiveStream = wr.GetResponseStream())
+            using (StreamReader sReader = new StreamReader(receiveStream, Encoding.UTF8))
+                content = sReader.ReadToEnd();
+            ChuckJokeModel chuckJoke = JsonSerializer.Deserialize<ChuckJokeModel>(content);
+            Console.WriteLine("");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(chuckJoke.value);
+            Console.WriteLine("");
+        }
+
+        static void BitcoinPrices()
+        {
+            string content = string.Empty;
+            string bitUrl = "https://api.coindesk.com/v1/bpi/currentprice.json";
+            WebRequest bitReq = WebRequest.Create(bitUrl);
+
+            using (WebResponse wr = bitReq.GetResponse())
+            using (Stream receiveStream = wr.GetResponseStream())
+            using (StreamReader sReader = new StreamReader(receiveStream, Encoding.UTF8))
+                content = sReader.ReadToEnd();
+            BitcoinPrice bitFact = JsonSerializer.Deserialize<BitcoinPrice>(content);
+            Console.WriteLine("");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("As Of - " + bitFact.time.updated);
+            Console.WriteLine("USD - $ " + bitFact.bpi.USD.rate);
+            Console.WriteLine("");
+        }
+
+        static void CatFact()
+        {
+            string content = string.Empty;
+            string catUrl = "https://cat-fact.herokuapp.com/facts/random";
+            WebRequest catReq = WebRequest.Create(catUrl);
+
+            using (WebResponse wr = catReq.GetResponse())
+            using (Stream receiveStream = wr.GetResponseStream())
+            using (StreamReader sReader = new StreamReader(receiveStream, Encoding.UTF8))
+                content = sReader.ReadToEnd();
+            CatFactModel catFact = JsonSerializer.Deserialize<CatFactModel>(content);
+            Console.WriteLine("");
+            if (catFact.status.verified)
+                Console.ForegroundColor = ConsoleColor.Yellow;
+            else
+                Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(catFact.text);
+            Console.WriteLine("");
+        }
+
+        static void Space()
+        {
+            string content = string.Empty;
+            string sapcePeopleUrl = "http://api.open-notify.org/astros.json";
+            WebRequest sapcePeopleReq = WebRequest.Create(sapcePeopleUrl);
+
+            using (WebResponse wr = sapcePeopleReq.GetResponse())
+            using (Stream receiveStream = wr.GetResponseStream())
+            using (StreamReader sReader = new StreamReader(receiveStream, Encoding.UTF8))
+                content = sReader.ReadToEnd();
+            SpacePersonModel sapcePeopleFact = JsonSerializer.Deserialize<SpacePersonModel>(content);
+            Console.WriteLine("");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("There are " + sapcePeopleFact.people.Count() + " in space right now!");
+            foreach (SpacePerson spacePerson in sapcePeopleFact.people)
+            {
+                Console.WriteLine(spacePerson.name + " is in " + spacePerson.craft);
+            }
+            Console.WriteLine("");
+        }
+
     }
+
+
 }
+© 2020 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Help
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
