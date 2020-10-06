d6ebf8f77546a4e20abe29005ede3ba6e5c5edb8
Irvin Dominin <irvin.dominin@sistemi.com>
2020-10-06 09:10:21 +0200

    Indentation a proxy on main
    In corporate development a proxy it's a daily pain

--- Models/CookieSuspectModel.cs
+++ Models/CookieSuspectModel.cs
@@ -1,21 +1,19 @@
-using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace UtilityBelt.Models
 {
-  public class CookieSuspectModel
-  {
-    public List<CookieSuspect> results { get; set; }
-  }
-  public class CookieSuspect
-  {
-    public CookieSuspectName name { get; set; }
-  }
-  public class CookieSuspectName
-  {
-    public string first { get; set; }
+    public class CookieSuspectModel
+    {
+        public List<CookieSuspect> results { get; set; }
+    }
+    public class CookieSuspect
+    {
+        public CookieSuspectName name { get; set; }
+    }
+    public class CookieSuspectName
+    {
+        public string first { get; set; }
 
-    public string last { get; set; }
-  }
+        public string last { get; set; }
+    }
 }
