9464ef682cbe7a944cddd34353431214fc917c2e
Ravi Lodhiya <rlodhiya2502@gmail.com>
2020-10-07 15:46:30 +0100

    Add Solution and Initialize xUnitTest Project

---
+++ Tests/UtilityBeltxUnitTests/PortScannerUnitTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace UtilityBeltxUnitTests
+{
+    public class PortScannerUnitTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
