55450bd7277e26ce4db4c3dbe8203b83b7bfa8ce
Ram Infosystems Limited <ravi@raminfosystems.co.uk>
2020-10-07 10:21:30 +0100

    Remove redundent assignment
    Remove redundent assignment and reformat it.

--- Program.cs
+++ Program.cs
@@ -813,58 +813,57 @@ namespace UtilityBelt
       Console.WriteLine();
     }
 
-    #endregion
+        #endregion
 
-    #region Console Calculator
-    static void ConsoleCalculator()
-    {
-      // Declare variables and then initialize to zero.
-      int num1 = 0; int num2 = 0;
+        #region Console Calculator
+        static void ConsoleCalculator()
+        {
 
-      // Display title as the C# console calculator app.
-      Console.WriteLine("Console Calculator in C#\r");
-      Console.WriteLine("------------------------\n");
+            // Display title as the C# console calculator app.
+            Console.WriteLine("Console Calculator in C#\r");
+            Console.WriteLine("------------------------\n");
 
-      // Ask the user to type the first number.
-      Console.WriteLine("Type a number, and then press Enter");
-      num1 = Convert.ToInt32(Console.ReadLine());
+            // Ask the user to type the first number.
+            Console.WriteLine("Type a number, and then press Enter");
+            // Declare variables and then initialize to zero.
+            int num1 = Convert.ToInt32(Console.ReadLine());
 
-      // Ask the user to type the second number.
-      Console.WriteLine("Type another number, and then press Enter");
-      num2 = Convert.ToInt32(Console.ReadLine());
+            // Ask the user to type the second number.
+            Console.WriteLine("Type another number, and then press Enter");
+            int num2 = Convert.ToInt32(Console.ReadLine());
 
-      // Ask the user to choose an option.
-      Console.WriteLine("Choose an option from the following list:");
-      Console.WriteLine("\ta - Add");
-      Console.WriteLine("\ts - Subtract");
-      Console.WriteLine("\tm - Multiply");
-      Console.WriteLine("\td - Divide");
-      Console.Write("Your option? ");
+            // Ask the user to choose an option.
+            Console.WriteLine("Choose an option from the following list:");
+            Console.WriteLine("\ta - Add");
+            Console.WriteLine("\ts - Subtract");
+            Console.WriteLine("\tm - Multiply");
+            Console.WriteLine("\td - Divide");
+            Console.Write("Your option? ");
 
-      // Use a switch statement to do the math.
-      switch (Console.ReadLine())
-      {
-        case "a":
-          Console.WriteLine($"Your result: {num1} + {num2} = " + (num1 + num2));
-          break;
-        case "s":
-          Console.WriteLine($"Your result: {num1} - {num2} = " + (num1 - num2));
-          break;
-        case "m":
-          Console.WriteLine($"Your result: {num1} * {num2} = " + (num1 * num2));
-          break;
-        case "d":
-          Console.WriteLine($"Your result: {num1} / {num2} = " + (num1 / num2));
-          break;
-      }
-      // Wait for the user to respond before closing.
-      Console.Write("Press any key to close the Calculator console app...");
-      Console.ReadKey();
-    }
-    #endregion
+            // Use a switch statement to do the math.
+            switch (Console.ReadLine())
+            {
+                case "a":
+                    Console.WriteLine($"Your result: {num1} + {num2} = " + (num1 + num2));
+                    break;
+                case "s":
+                    Console.WriteLine($"Your result: {num1} - {num2} = " + (num1 - num2));
+                    break;
+                case "m":
+                    Console.WriteLine($"Your result: {num1} * {num2} = " + (num1 * num2));
+                    break;
+                case "d":
+                    Console.WriteLine($"Your result: {num1} / {num2} = " + (num1 / num2));
+                    break;
+            }
+            // Wait for the user to respond before closing.
+            Console.Write("Press any key to close the Calculator console app...");
+            Console.ReadKey();
+        }
+        #endregion
 
-    #region Gender from name
-    static void GenderFromName()
+        #region Gender from name
+        static void GenderFromName()
     {
       Console.Write("Type the name, and then press Enter: ");
       string enteredName = Console.ReadLine().ToLower();
