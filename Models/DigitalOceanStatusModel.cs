f3b5810da1babe05f10add78f603071b4b2f2d3d
Irvin Dominin <irvin.dominin@sistemi.com>
2020-10-07 09:46:57 +0200

    Fixed indentation and case 28 - DigitalOcean status

---
+++ Models/DigitalOceanStatusModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace UtilityBelt.Models
+{
+    class DigitalOceanStatusModel
+    {
+        [JsonPropertyName("page")]
+        public DigitalOceanStatusPageModel Page { get; set; }
+        [JsonPropertyName("status")]
+        public DigitalOceanStatusStatusModel Status { get; set; }
+    }
+
+    class DigitalOceanStatusPageModel
+    {
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+        [JsonPropertyName("url")]
+        public string Url { get; set; }
+        [JsonPropertyName("time_zone")]
+        public string TimeZone { get; set; }
+        [JsonPropertyName("updated_at")]
+        public DateTime LastUpdate { get; set; }
+    }
+
+    class DigitalOceanStatusStatusModel
+    {
+        [JsonPropertyName("indicator")]
+        public string Indicator { get; set; }
+        [JsonPropertyName("description")]
+        public string Description { get; set; }
+    }
+}
