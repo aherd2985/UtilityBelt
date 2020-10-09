9f31386b5efe28db8d48dcc67ada2d5606e3bb4d
jacobcampbell707 <jacobcampbell707@gmail.com>
2020-10-08 21:32:21 -0500

    building fbi model

---
+++ Models/FBITopTenModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace UtilityBelt.Models
+{
+  public class FBITopTenModel
+  {
+
+    
+  }
+  public class FBITopTenSuspect
+  {
+    [JsonPropertyName("age_range")]
+    public string AgeRange { get; set; }
+    [JsonPropertyName("weight")]
+    public string Weight { get; set; }
+    [JsonPropertyName("occupations")]
+    public string Occupations { get; set; }
+    [JsonPropertyName("locations")]
+    public string Locations { get; set; }
+    [JsonPropertyName("reward_text")]
+    public string Reward { get; set; }
+    [JsonPropertyName("hair")]
+    public string Hair { get; set; }
+    [JsonPropertyName("")]
+    public string DatesOfBirthUsed { get; set; }
+    public string Nationality { get; set; }
+    public string ScarsAndMarks { get; set; }
+    public string Aliases { get; set; }
+    public string FieldOffice { get; set; }
+    
+  }
+}
---
+++ Utilities/FBITopTen.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Composition;
+using System.Net;
+using System.Text;
+using UtilityBelt.Models;
+
+namespace UtilityBelt.Utilities
+{
+  [Export(typeof(IUtility))]
+  internal class FBITopTen
+  {
+    public IList<string> Commands => new List<string> { "FBI", "FBI Top Ten" };
+    public string Name => "FBI Top Ten Most Wanted";
+
+    public void Configure(IOptions<SecretsModel> options)
+    {
+
+    }
+
+    public void Run()
+    {
+      string content = string.Empty;
+      string fbiUrl = "https://api.fbi.gov/wanted/v1/list";
+      using (var wc = new WebClient())
+      {
+        content = wc.DownloadString(fbiUrl);
+      }
+    }      
+  }
+}
