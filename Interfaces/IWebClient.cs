c20f6eaa27cbda9d0068b963a300f1ff2d729d2d
1RedOne <stephen@foxdeploy.com>
2020-10-10 09:21:35 -0400

    adds an interface to make testing easy

---
+++ Interfaces/IWebClient.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UtilityBelt.Interfaces
+{
+  public interface IWebClient : IDisposable
+  {
+    // Required methods (subset of `System.Net.WebClient` methods).
+    byte[] DownloadData(Uri address);
+
+    byte[] UploadData(Uri address, byte[] data);
+  }
+
+  internal interface IWebClientFactory
+  {
+    IWebClient Create();
+  }
+}
