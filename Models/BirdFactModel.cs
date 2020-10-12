a96429d180c4f6e28bf703e50f39f318b6b62b1d
1RedOne <stephen@foxdeploy.com>
2020-10-12 11:01:26 -0400

    adds BirdFact utility and tests

---
+++ Models/BirdFactModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace UtilityBelt.Models
+{
+  public class BirdFactModel
+  {
+    [JsonPropertyName("fact")]
+    public string Fact { get; set; }
+  }
+}
---
+++ Tests/UtilityBeltxUnitTests/BirdFactsTests.cs
@@ -0,0 +1,64 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.IO;
+using System.Net;
+using System.Text.Json;
+using UtilityBelt;
+using UtilityBelt.Interfaces;
+using UtilityBelt.Models;
+using UtilityBelt.Utilities;
+using Xunit;
+
+namespace UtilityBeltxUnitTests
+{
+  public class BirdFactTests
+  {
+    private readonly Mock<IWebClient> webClient = new Mock<IWebClient>();
+
+    public BirdFactTests()
+    {
+      var mockBirdQuote = getBirdFact();
+      var jsonresult = JsonSerializer.Serialize(mockBirdQuote);
+      this.webClient.Setup(x => x.DownloadString(It.IsAny<string>())).Returns(jsonresult);
+    }
+
+    [Fact]
+    public void BirdFactReturnsBirdFacts()
+    {
+      var birdClient = new TestableBirdFact(webClient.Object);
+      birdClient.Run();
+      this.webClient.Verify(x => x.DownloadString(It.IsAny<string>()), Times.Once);
+    }
+
+    [Fact]
+    public void BirdFactDoesNotThrowsIfNoResult()
+    {
+      this.webClient.Setup(x => x.DownloadString(It.IsAny<string>())).Returns("");
+
+      var birdClient = new TestableBirdFact(webClient.Object);
+      birdClient.Run();
+      this.webClient.Verify(x => x.DownloadString(It.IsAny<string>()), Times.Once);
+    }
+
+    private BirdFactModel getBirdFact()
+    {
+      var fileName = Path.Combine("responseSamples", "birdFactResponse.json");
+      string jsonString = File.ReadAllText(fileName);
+
+      return JsonSerializer.Deserialize<BirdFactModel>(jsonString);
+    }
+
+    private class TestableBirdFact : CatFact
+    {
+      public IWebClient webClient { get; set; }
+
+      public TestableBirdFact(IWebClient webClient) : base()
+      {
+        this.webClient = webClient;
+      }
+
+      protected override IWebClient GetWebClient() => this.webClient;
+    }
+  }
+}
