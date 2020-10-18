0d90106207306c2138179ef4215754acd19f1916
cesar.calva <cesar.cruz@acftechnologies.com>
2020-10-17 19:43:03 -0500

    Included Yoda translation module

---
+++ Models/YodaTranslationModel.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+namespace UtilityBelt.Models
+{
+  public class YodaTranslationModel
+  {
+    [JsonPropertyName("contents")]
+    public Contents contents { get; set; }
+  }
+
+  public class Contents
+  {
+    public string translated { get; set; }
+
+    public string text { get; set; }
+
+    public string translation { get; set; }
+  }
+}
