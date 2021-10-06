f85dbbc74a7d14bfb248609c4f5bfcf60e08254d
Irvin Dominin <irvin.dominin@sistemi.com>
2021-10-06 22:03:07 +0200

    Added Agify API

---
+++ Models/AgifyModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace UtilityBelt.Models
+{
+  public class AgifyModel
+  {
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+    [JsonPropertyName("age")]
+    public int Age { get; set; }
+    [JsonPropertyName("count")]
+    public long Count { get; set; }
+  }
+}
---
+++ Utilities/Agify.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Composition;
+using System.Net;
+using System.Text.Json;
+using UtilityBelt.Models;
+
+namespace UtilityBelt.Utilities
+{
+  [Export(typeof(IUtility))]
+  internal class Agify : IUtility
+  {
+    public IList<string> Commands => new List<string> { "agify" };
+
+    public string Name => "Agify";
+
+    public void Configure(IOptions<SecretsModel> options)
+    {
+    }
+
+    public void Run()
+    {
+      string content = string.Empty;
+
+      Console.WriteLine("Please enter a name  to guess its age");
+      String userInput = Console.ReadLine();
+
+      string agifyURL = $"https://api.agify.io/?name={userInput}";
+      using (var wc = new WebClient())
+      {
+        content = wc.DownloadString(agifyURL);
+      }
+
+      AgifyModel agify = JsonSerializer.Deserialize<AgifyModel>(content);
+      Console.WriteLine();
+      Console.WriteLine(@$"Age -- {agify.Age}");
+    }
+  }
+}
