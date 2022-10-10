d5b7ddfb2c7a7e0d526a0de07602d18d7581c3b6
Irvin Dominin <irvin.dominin@sistemi.com>
2022-10-10 21:47:07 +0200

    Added RandomCommit text

---
+++ Utilities/RandomCommit.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Composition;
+using System.Net;
+using System.Text.Json;
+using UtilityBelt.Models;
+
+namespace UtilityBelt.Utilities
+{
+  [Export(typeof(IUtility))]
+  internal class RandomCommit : IUtility
+  {
+    public IList<string> Commands => new List<string> { "commit" };
+
+    public string Name => "Random Commit Text";
+
+    public void Configure(IOptions<SecretsModel> options)
+    {
+    }
+
+    public void Run()
+    {
+      string content;
+      string url = "http://whatthecommit.com/index.txt";
+      using (var wc = new WebClient())
+      {
+        content = wc.DownloadString(url);
+      }
+
+      Console.WriteLine();
+      Console.ForegroundColor = ConsoleColor.Red;
+      Console.WriteLine(content);
+      Console.WriteLine();
+    }
+  }
+}
