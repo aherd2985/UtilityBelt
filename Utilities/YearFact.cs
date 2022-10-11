e2d4d6d785368ae115ed323b2042c1efdc493f56
Irvin Dominin <irvin.dominin@sistemi.com>
2022-10-11 18:13:12 +0200

    YearFact api

---
+++ Utilities/YearFact.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Composition;
+using System.Net;
+using System.Text.Json;
+using UtilityBelt.Models;
+
+namespace UtilityBelt.Utilities
+{
+  [Export(typeof(IUtility))]
+  internal class YearFact : IUtility
+  {
+    public IList<string> Commands => new List<string> { "yearfact" };
+
+    public string Name => "Year Fact";
+
+    public void Configure(IOptions<SecretsModel> options)
+    {
+    }
+
+    public void Run()
+    {
+      string content = string.Empty;
+
+      string randomYearFactURL = $"http://numbersapi.com/random/year?format=json";
+      using (var wc = new WebClient())
+      {
+        content = wc.DownloadString(randomYearFactURL);
+      }
+
+      Console.WriteLine(content);
+
+    }
+  }
+}
