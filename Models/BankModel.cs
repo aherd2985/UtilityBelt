cda241609521dc1c6a1c386cecdaae0188763e7c
Irvin Dominin <irvin.dominin@sistemi.com>
2023-10-04 08:03:32 +0200

    Added random bank generator

---
+++ Models/BankModel.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+namespace UtilityBelt.Models
+{
+  public class BankModel
+  {
+    [JsonPropertyName("account_number")]
+    public string AccountNumber { get; set; }
+    [JsonPropertyName("iban")]
+    public string IBAN { get; set; }
+    [JsonPropertyName("bank_name")]
+    public string BankName { get; set; }
+    [JsonPropertyName("routing_number")]
+    public string RoutingNumber { get; set; }
+    [JsonPropertyName("swift_bic")]
+    public string SwiftBic { get; set; }
+  }
+
+}
