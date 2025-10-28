a57d872f0f31e7c20d722201c6a504309fdf917f
Irvin Dominin <irvin.dominin@sistemi.com>
2025-10-28 15:06:06 +0100

    Added Potter character

---
+++ Models/PotterCharactersModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace UtilityBelt.Models
+{
+  public class PotterCharactersModel
+  {
+    [JsonPropertyName("fullName")]
+    public string FullName { get; set; }
+    [JsonPropertyName("nickName")]
+    public string Nickname { get; set; }
+    [JsonPropertyName("hogwartsHouse")]
+    public string HogwartsHouse { get; set; }
+    [JsonPropertyName("interpretedBy")]
+    public string InterpretedBy { get; set; }
+  }
+}
---
+++ Utilities/PotterCharacters.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Composition;
+using System.Net;
+using System.Text.Json;
+using UtilityBelt.Models;
+
+namespace UtilityBelt.Utilities
+{
+  [Export(typeof(IUtility))]
+  internal class PotterCharacters : IUtility
+  {
+    public IList<string> Commands => new List<string> { "potter" };
+
+    public string Name => "Harry Potter - Characters search";
+
+    public void Configure(IOptions<SecretsModel> options)
+    {
+    }
+
+    public void Run()
+    {
+      string content = string.Empty;
+
+      Console.WriteLine("Please enter a name a character name to search");
+      String userInput = Console.ReadLine();
+
+      if (string.IsNullOrEmpty(userInput))
+      {
+        Console.WriteLine("No character is provided!");
+        Console.WriteLine();
+        return;
+      }
+
+      string potterURL = $"https://potterapi-fedeperin.vercel.app/es/characters?search={userInput}";
+      using (var wc = new WebClient())
+      {
+        content = wc.DownloadString(potterURL);
+      }
+
+      PotterCharactersModel[] potter = JsonSerializer.Deserialize<PotterCharactersModel[]>(content);
+      Console.WriteLine();
+      foreach (var character in potter)
+      {
+        Console.WriteLine(@$"Name -- {character.FullName}");
+        Console.WriteLine(@$"Nickname -- {character.Nickname}");
+        Console.WriteLine(@$"Hogwarts House -- {character.HogwartsHouse}");
+        Console.WriteLine();
+      }
+    }
+  }
+}
